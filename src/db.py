0b954449d0c31c7cd7a1202542a7a8fa8ba74617
emilypenna <ep439@cornell.edu>
2022-04-30 16:26:31 -0400

    added users DAO helper file and authentication methods to User model

--- src/db.py
+++ src/db.py
@@ -1,4 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+import datetime
+import hashlib
+import os
+import bcrypt
 
 db = SQLAlchemy()
 
@@ -31,6 +35,15 @@ class User(db.Model):
     ta_courses = db.relationship("Course", secondary= ta_association_table, back_populates="tas")
     office_hours = db.relationship("OfficeHours", cascade="delete")
 
+    # User information
+    email = db.Column(db.String, nullable=False, unique=True)
+    password_digest = db.Column(db.String, nullable=False)
+
+    # Session information. Here there is only one session
+    session_token = db.Column(db.String, nullable=False, unique=True)
+    session_expiration = db.Column(db.DateTime, nullable=False)
+    update_token = db.Column(db.String, nullable=False, unique=True)
+
     def __init__(self, **kwargs): 
         """
         initalize user object/entry
@@ -38,6 +51,10 @@ class User(db.Model):
         self.name = kwargs.get("name", "")
         self.netid = kwargs.get("netid", "")
         self.is_ta = kwargs.get("is_ta", False)    #by default a user is not a ta
+
+        self.email = kwargs.get("email")
+        self.password_digest = bcrypt.hashpw(kwargs.get("password").encode("utf8"), bcrypt.gensalt(rounds=13))
+        self.renew_session()
 
     def serialize(self):
         """
@@ -60,6 +77,44 @@ class User(db.Model):
             "name": self.name,
             "netid": self.netid,
         }
+
+    #authentication methods:-----------------------------------------------------------------------------------------------------
+
+    def _urlsafe_base_64(self):
+        """
+        Randomly generates hashed tokens (used for session/update tokens)
+        """
+        return hashlib.sha1(os.urandom(64)).hexdigest()
+
+    def renew_session(self):
+        """
+        Renews the sessions, i.e.
+        1. Creates a new session token
+        2. Sets the expiration time of the session to be a day from now
+        3. Creates a new update token
+        """
+        self.session_token = self._urlsafe_base_64()
+        #define what the session experiation time is
+        self.session_expiration = datetime.datetime.now() + datetime.timedelta(days=1)
+        self.update_token = self._urlsafe_base_64()
+
+    def verify_password(self, password):
+        """
+        Verifies the password of a user
+        """
+        return bcrypt.checkpw(password.encode("utf8"), self.password_digest)
+
+    def verify_session_token(self, session_token):
+        """
+        Verifies the session token of a user
+        """
+        return session_token == self.session_token and datetime.datetime.now() < self.session_expiration
+
+    def verify_update_token(self, update_token):
+        """
+        Verifies the update token of a user
+        """
+        return update_token == self.update_token
 
 #----COURSE MODEL-------------------------------------------------------------------------------
 
